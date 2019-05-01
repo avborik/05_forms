c08a69fe0663fadd84055bf913e8402f5a5d1f50
AvBorikDeveloper <avboris28@gmail.com>
2019-05-01 23:49:48 +1200

    controlled and uncontrolled

--- src/components/controlled.js
+++ src/components/controlled.js
@@ -3,13 +3,53 @@ import React, { Component } from 'react';
 class Controlled extends Component {
 
     state = {
-        
+        name:'',
+        lastname:''
+    }
+
+    handleNameChange = (event) => {
+            this.setState({
+                name:event.target.value
+            })
+    }
+
+    handleLastNameChange = (event) => {
+        this.setState({
+            lastname:event.target.value
+        })
+    }
+
+    onshandler = (event) => {
+        event.preventDefault();
+
+        console.log(this.state)
     }
 
     render(){
         return(
-            <div>
-                Controlled
+            <div className="container">
+                <form onSubmit={this.onshandler}>
+                    <div className="form-element">
+                    <label>Enter name</label>
+                    <input
+                        type="text"
+                        onChange={this.handleNameChange}
+                        value={this.state.name}
+                        />
+
+                    </div>
+
+                    <div className="form_element">
+                        <label>Enter Lastname</label>
+                        <input
+                            type="text"
+                            onChange={this.handleLastNameChange}
+                            value={this.state.lastname}
+                            />
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
+                
             </div>
         )
     }
